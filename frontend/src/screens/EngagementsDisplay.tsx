c31a47bd723c6753686a44f5d13f080d4e57c817
Triston Pang <triston@span.io>
2020-10-08 20:51:29 +0800

    Mobile-ify EngagementDisplay page (DSOAP-53)

--- frontend/src/screens/EngagementsDisplay.tsx
+++ frontend/src/screens/EngagementsDisplay.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MenuIcon from '@material-ui/icons/Menu';
 import { createStyles, makeStyles, Theme, ThemeProvider } from '@material-ui/core/styles';
-import { AppBar, Toolbar, IconButton, Typography, Box, Grid, CircularProgress } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Typography, Box, Grid } from '@material-ui/core';
 import InfiniteScroll from 'react-infinite-scroll-component'
-import { useLazyQuery, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
+import { useMediaQuery } from 'react-responsive';
 
 import EngagementCard from '../components/EngagementCard';
 import appTheme from '../theme/globalTheme';
@@ -20,16 +21,31 @@ const useStyles = makeStyles((theme: Theme) =>
       alignItems: 'flex-start',
       marginLeft: '16%',
       marginRight: '16%',
-      marginTop: '8%',
+      marginTop: '16%',
       marginBottom: '8%',
       paddingRight: '36%',
     },
+    startTextContainerMobile: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      marginLeft: '8%',
+      marginRight: '8%',
+      marginTop: '20%',
+      marginBottom: '8%',
+      paddingTop: 56,
+    },
     startTitle: {
       fontWeight: 700,
+      textAlign: 'left',
     },
     startSubtext: {
       textAlign: 'left',
       marginTop: 40,
+    },
+    startSubtextMobile: {
+      textAlign: 'left',
+      marginTop: 20,
     },
     engagementsGridContainer: {
       backgroundColor: '#f3f3f7',
@@ -46,6 +62,13 @@ const useStyles = makeStyles((theme: Theme) =>
       marginLeft: '16%',
       width: '50%',
     },
+    footerContainerMobile: {
+      height: 255,
+      display: 'flex',
+      flexDirection: 'column',
+      marginLeft: '16%',
+      marginTop: 26,
+    },
     footerPartition: {
       display: 'flex',
       flexDirection: 'column',
@@ -53,8 +76,13 @@ const useStyles = makeStyles((theme: Theme) =>
       justifyContent: 'center',
       marginRight: '5%'
     },
+    footerPartitionMobile: {
+      marginRight: '5%',
+      marginBottom: 12,
+    },
     fadedText: {
-      color: 'rgba(0,0,0,0.87)'
+      color: 'rgba(0,0,0,0.87)',
+      textAlign: 'left',
     },
     infiniteScroll: {
       display: 'flex',
@@ -63,16 +91,27 @@ const useStyles = makeStyles((theme: Theme) =>
     engagementGrid: {
       display: 'flex',
       justifyContent: 'center',
-    }
+      width: '100%',
+    },
+    footerLogo: {
+      marginBottom: 0,
+    },
+    footerLogoMobile: {
+      marginBottom: 16,
+      textAlign: 'left',
+    },
   }),
 );
 
 const EngagementsDisplay = () => {
+  // Media queries
+  const isMobile = useMediaQuery({ query: '(max-width: 1224px)' });
+
   const [engagements, setEngagements] = useState<EngagementFields[]>([]);
   const [isLoadExisting, setIsLoadExisting] = useState<boolean>(true);
   const [lastEngagementId, setLastEngagementId] = useState<string>('');
   const [skipQuery, setSkipQuery] = useState<boolean>(true);
-  const { loading } = useQuery<GetAssignmentsData, GetAssignmentsVariables>(
+  useQuery<GetAssignmentsData, GetAssignmentsVariables>(
     GET_ASSIGNMENTS,
     {
       variables: { startId: lastEngagementId, perPage: ENGAGEMENTS_PER_PAGE },
@@ -92,7 +131,7 @@ const EngagementsDisplay = () => {
   );
   const loadData = () => {
     setSkipQuery(false);
-  }
+  };
 
   // Initial load/query
   useEffect(loadData, []);
@@ -108,13 +147,13 @@ const EngagementsDisplay = () => {
           </IconButton>
           <Typography variant="h6" >
             voltch
-        </Typography>
+          </Typography>
         </Toolbar>
       </AppBar>
-      <Box className={classes.startTextContainer}>
-        <Typography variant='h3' className={classes.startTitle}>Volunteer,</Typography>
-        <Typography variant='h3' className={classes.startTitle}>one hour at a time</Typography>
-        <Typography variant='body1' className={classes.startSubtext}>
+      <Box className={isMobile ? classes.startTextContainerMobile : classes.startTextContainer}>
+        <Typography variant={isMobile ? 'h4' : 'h3'} className={classes.startTitle}>Volunteer,</Typography>
+        <Typography variant={isMobile ? 'h4' : 'h3'} className={classes.startTitle}>one hour at a time</Typography>
+        <Typography className={isMobile ? classes.startSubtextMobile : classes.startSubtext}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           Nam in faucibus justo. Sed placerat justo eu turpis posuere ultricies.
           Curabitur at arcu ac mauris laoreet fermentum at ut leo.
@@ -127,16 +166,17 @@ const EngagementsDisplay = () => {
           next={loadData}
           dataLength={engagements.length}
           hasMore={isLoadExisting}
-          loader={<p>LOADING</p>}
+          loader={<div />}
           className={classes.infiniteScroll}
         >
           {/* TODO: The grid isnt properly formatted - loader is appearing in a weird position next to all 
           the other engagements instead of directly below */}
+          {/* Triston: I have temporarily changed the loader into an empty div */}
           <Grid
             container
             direction="row"
             wrap="wrap"
-            xs={8}
+            xs={isMobile ? 12 : 8}
             className={classes.engagementGrid}
           >
             {engagements.map(({ title, _id }) => {
@@ -146,13 +186,13 @@ const EngagementsDisplay = () => {
         </InfiniteScroll>
       </Box>
 
-      <Box className={classes.footerContainer}>
-        <Box className={classes.footerPartition}>
-          <Typography variant='h6'>voltch</Typography>
+      <Box className={isMobile ? classes.footerContainerMobile : classes.footerContainer}>
+        <Box className={isMobile ? classes.footerPartitionMobile : classes.footerPartition}>
+          <Typography variant='h6' className={isMobile ? classes.footerLogoMobile : classes.footerLogo}>voltch</Typography>
           <Typography className={classes.fadedText}>In collaboration with</Typography>
           <Typography className={classes.fadedText}><strong>NUS Disability Support Office</strong></Typography>
         </Box>
-        <Box className={classes.footerPartition}>
+        <Box className={isMobile ? classes.footerPartitionMobile : classes.footerPartition}>
           <Typography align='left' className={classes.fadedText}>National University of Singapore</Typography>
           <Typography align='left' className={classes.fadedText}>Yusof Ishak House, Level 3</Typography>
           <Typography align='left' className={classes.fadedText}>31 Lower Kent Ridge Road</Typography>
